59297451c2c4f6232168b9d4cdb794d92cb7ee46
mornhussakuyo-hub <mornhussakuyo@gmail.com>
2026-05-28 21:03:42 +0800

    更新贴表情指令

--- src/luoying_bot/application/commands/builtin.py
+++ src/luoying_bot/application/commands/builtin.py
@@ -5,6 +5,26 @@ from luoying_bot.application.commands.base import BaseCommand
 from luoying_bot.domain.context import ChatContext
 from luoying_bot.domain.result import Reply
 
+OFFICIAL_EMOJIS = [
+    4, 5, 8, 9, 10, 12, 14, 16, 21, 23, 24, 25, 26, 27, 28, 29,
+    30, 32, 33, 34, 38, 39, 41, 42, 43, 49, 53, 60, 63, 66, 74,
+    75, 76, 78, 79, 85, 89, 96, 97, 98, 99, 100, 101, 102, 103,
+    104, 106, 109, 111, 116, 118, 120, 122, 123, 124, 125, 129,
+    144, 147, 171, 173, 174, 175, 176, 179, 180, 181, 182, 183,
+    201, 203, 212, 214, 219, 222, 227, 232, 240, 243, 246, 262,
+    264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 277, 278,
+    281, 282, 284, 285, 287, 289, 290, 293, 294, 297, 298, 299,
+    305, 306, 307, 314, 315, 318, 319, 320, 322, 324, 326,
+    9728, 9749, 9786, 10024, 10060, 10068, 127801, 127817,
+    127822, 127827, 127836, 127838, 127847, 127866, 127867,
+    127881, 128027, 128046, 128051, 128053, 128074, 128076,
+    128077, 128079, 128089, 128102, 128104, 128147, 128157,
+    128164, 128166, 128168, 128170, 128235, 128293, 128513,
+    128514, 128516, 128522, 128524, 128527, 128530, 128531,
+    128532, 128536, 128538, 128540, 128541, 128557, 128560,
+    128563
+]
+
 #测试通过
 class HelpCommand(BaseCommand):
     name = '/help'
@@ -177,12 +197,33 @@ class DisWholeBanCommand(BaseCommand):
         return Reply(text='已关闭全员禁言')
 
 #测试通过
-class AttachCommand(BaseCommand):
-    name = '/attach'
+class EmojiCommand(BaseCommand):
+    name = '/emoji'
+    args_required = True
+    required_args = {'--code': ['-c']}
+    async def validate(self, args):
+        if not args['--code'].isdigit(): 
+            raise ValueError('--id 必须是 TransportEmojiCode')
+
+        return args
+    async def execute(self, context, args):
+        try:
+            await self.services.transport.send_reaction(context, emoji_id=args['--code'])
+        except Exception:
+            pass
+
+        if int(args['--code']) not in OFFICIAL_EMOJIS:
+            return Reply(text='未知的表情代码，不保证可发出', silent=True)
+
+        return Reply(text='', silent=True)
+
+
+class EmojiListCommand(BaseCommand):
+    name = '/emoji_list'
     async def validate(self, args): return args
     async def execute(self, context, args):
-        await self.services.transport.send_reaction(context, emoji_id=297)
-        return Reply(text='', silent=True)
+        return Reply(text=f"表情列表：{https://bot.q.qq.com/wiki/develop/api-v2/openapi/emoji/model.html#EmojiType}")
+
 
 #测试通过
 class DiceCommand(BaseCommand):
